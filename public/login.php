6262b584cf353142dbea548adb2fa0a86933718b
mizryl <pham_linda@hotmail.com>
2025-03-27 14:14:52 -0700

    Create login.php
    Rough inputs

---
+++ public/login.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gourmet Destination | Login Page</title>
+</head>
+<body>
+    <!-- Login Form -->
+    <form>
+        <input type="email" placeholder="Email" name="email">
+        <input type="password" placeholder="Password" name="password">
+        <button type="submit" placeholder="Password">Login</button>
+        <p>Don't have an account? <a href="signup.php">Register</a></p>
+    </form>
+    
+</body>
+</html>
