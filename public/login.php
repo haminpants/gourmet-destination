2225e2d1be0f508cf00573bf27a290af7c131386
mizryl <pham_linda@hotmail.com>
2025-03-27 18:44:05 -0700

    Login Implementation

--- public/login.php
+++ public/login.php
@@ -1,4 +1,8 @@
 <?php
+session_unset();
+session_destroy();
+session_start();
+
 
 
 ?>
@@ -12,13 +16,19 @@
     <title>Gourmet Destination | Login Page</title>
 </head>
 <body>
+    
     <!-- Login Form -->
-    <form>
-        <input type="email" placeholder="Email" name="email">
-        <input type="password" placeholder="Password" name="password">
-        <button type="submit" placeholder="Password">Login</button>
-        <p>Don't have an account? <a href="signup.php">Register</a></p>
-    </form>
+    <div id="loginForm">
+        <p><?php echo $error ?></p>
+        <form method="POST" action="login-action.php">
+            <label>Email</label><br>
+            <input type="email" placeholder="Email" name="email"> <br>
+            <label>Password</label><br>
+            <input type="password" placeholder="Password" name="password"> <br>
+            <button type="submit" placeholder="Password">Login</button>
+            <p>Don't have an account? <a href="signup.php">Register</a></p>
+        </form>
+    </div>
     
 </body>
 </html>
--- public/navbar.php
+++
@@ -1,4 +0,0 @@
-<nav>
-    <a href="login.php">Login</a>
-    <a href="signup.php">Sign Up</a>
-</nav>
