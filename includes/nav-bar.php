71194c8be0eb5205e3412e993dfa732fdeaf0ca5
haminpants <samueltfong03@gmail.com>
2025-03-30 05:45:18 -0700

    update nav
    add link to home page
    remove logout link

--- includes/nav-bar.php
+++ includes/nav-bar.php
@@ -5,11 +5,16 @@ if (session_status() === PHP_SESSION_NONE) session_start();
 ?>
 
 <nav class="nav-bar">
-    <?php if (empty($_SESSION["userData"])) { ?>
-        <a href="login.php">Log In</a>
-        <a href="sign-up.php">Sign Up</a>
-    <?php } else { ?>
-        <a href="profile.php">My Profile</a>
-        <a href="actions/logout-action.php">Log Out</a>
-    <?php } ?>
+    <div class="left">
+        <a href="index.php">Home</a>
+    </div>
+    <div class="right">
+        <?php if (empty($_SESSION["userData"])) { ?>
+            <a href="login.php">Log In</a>
+            <a href="sign-up.php">Sign Up</a>
+        <?php } else { ?>
+            <a href="profile.php">My Profile</a>
+        <?php } ?>
+    </div>
 </nav>
+<div class="nav-spacer"></div>
