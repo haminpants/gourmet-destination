04ef2d5c8e00e285ec913f74007d1adfd15e9315
mizryl <pham_linda@hotmail.com>
2025-03-30 02:29:04 -0700

    few changes

--- public/search.php
+++ public/search.php
@@ -11,12 +11,14 @@ session_start();
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
-    <div>
+    <div id="searchPage">
+        <button type="button"><a href='index.php'>Back</a></button><br>
         <?php
         //fetches data from query that's stored in a session
             if (isset($_SESSION['searchData']) && (!empty($_SESSION['searchData']))) {
                 foreach ($_SESSION['searchData'] as $user) {
                     echo '<div id="searchUsers">';
+                    echo !empty($user['profile_picture']) ? htmlspecialchars($user['profile_picture']) : '';
                     echo htmlspecialchars($user['first_name']) . " " . htmlspecialchars($user['last_name']) . "<br>";
                     echo htmlspecialchars($user['country_id']) . ", " . htmlspecialchars($user['subdivision_id']) . "<br>";
                     echo htmlspecialchars($user['name']) . "<br>";
