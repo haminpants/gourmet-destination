26f529112a861c9666d660a28520222986d9aa2d
mizryl <pham_linda@hotmail.com>
2025-03-31 10:20:32 -0700

    update

--- public/actions/submit-reviews-action.php
+++ public/actions/submit-reviews-action.php
@@ -3,9 +3,13 @@
 if ($_SERVER['REQUSET_METHOD'] === "POST") {
     // Get the rating values from the form
     $rating = isset($_POST['rating']) ? $_POST['rating'] : null;
+    $comment = isset($_POST['comment']) ? $_POST['comment'] : null;
 
-    if ($rating) {
-        $stmt = $pdo->prepare("INSERT INTO ratings")
+    if ($rating || $comment) {
+        $stmt = $pdo->prepare("INSERT INTO reviews (user_id. rating, description, created_at) 
+                                VALUES (:user_id, :rating, :description, NOW());
+                                ");
+        $stmt->execue([':user_id' => $_GET('user_id'), ':rating' => $rating, ':description' => $comment]);
     }
 
 }
--- public/review.php
+++ public/review.php
@@ -11,15 +11,14 @@
     <div class="review">
         <p>Thank you for booking through our platform!</p>
         <p>Leave a rating for your host!</p>
-        <!-- <span class="stars"><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i> -->
 
         <div class="star-rating">
             <form method= "POST" action="actions/submit-reviews-action.php">
-                <input type="radio" id="star1" name="rating" value="1"/><label for="star1">&#9734;</label>
-                <input type="radio" id="star2" name="rating" value="2"/><label for="star2">&#9734;</label>
-                <input type="radio" id="star3" name="rating" value="3"/><label for="star3">&#9734;</label>
-                <input type="radio" id="star4" name="rating" value="4"/><label for="star4">&#9734;</label>
-                <input type="radio" id="star5" name="rating" value="5"/><label for="star5">&#9734;</label>
+                <input type="radio" id="star1" name="rating" value="1"/><label for="star1">&#9733;</label>
+                <input type="radio" id="star2" name="rating" value="2"/><label for="star2">&#9733;</label>
+                <input type="radio" id="star3" name="rating" value="3"/><label for="star3">&#9733;</label>
+                <input type="radio" id="star4" name="rating" value="4"/><label for="star4">&#9733;</label>
+                <input type="radio" id="star5" name="rating" value="5"/><label for="star5">&#9733;</label>
 
             </form>
             <button type="submit">Submit Ratings</button>
