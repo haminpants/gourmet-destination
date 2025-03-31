8fdc19e0afe399e267635294fe55d1f443e784cc
mizryl <pham_linda@hotmail.com>
2025-03-31 01:06:00 -0700

    Review in progress

---
+++ public/review.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gourmet Destination | Reviews & Rating</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div>
+        <p>Thank you for booking through our platform!</p>
+        <p>Leave a rating for your host!</p>
+        
+
+    </div>
+</body>
+</html>
