d2f36a1753730916aaa967db462de0b56d756346
haminpants <samueltfong03@gmail.com>
2025-03-30 23:40:39 -0700

    clean up code

--- public/profile.php
+++ public/profile.php
@@ -8,24 +8,25 @@
 
     <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body class="profile-page">
-<?php
-session_start();
-require "../src/db.php";
+    <?php
+    session_start();
+    require "../src/db.php";
 
-if (isset($_GET["id"]) && is_numeric($_GET["id"])) $profileData = getUserById($pdo, intval($_GET["id"]));
-else if (isset($_SESSION["userData"])) $profileData = getUserById($pdo, $_SESSION["userData"]["id"]);
-else header("Location: login.php");
+    if (isset($_GET["id"]) && is_numeric($_GET["id"])) $profileData = getUserById($pdo, intval($_GET["id"]));
+    else if (isset($_SESSION["userData"])) $profileData = getUserById($pdo, $_SESSION["userData"]["id"]);
+    else header("Location: login.php");
 
-if (!empty($profileData)) {
-    include "../includes/nav-bar.php";
-    include "../includes/profiles/profile-banner.php";
-    include "../includes/profiles/profile-edit.php";
-    include "../includes/profiles/profile-experience-display.php";
-    include "../includes/experiences/experience-info-form.php";
-} else include "../includes/profiles/no-profile-found.php";
-?>
-  
+    if (!empty($profileData)) {
+        include "../includes/nav-bar.php";
+        include "../includes/profiles/profile-banner.php";
+        include "../includes/profiles/profile-edit.php";
+        include "../includes/profiles/profile-experience-display.php";
+        include "../includes/experiences/experience-info-form.php";
+    } else include "../includes/profiles/no-profile-found.php";
+    ?>
+
 </body>
 
 </html>
