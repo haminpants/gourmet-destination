67121590d8d2698aec06d1368af4ad4f116c84e6
mizryl <pham_linda@hotmail.com>
2025-03-31 13:40:14 -0700

    integration in progress

---
+++ public/actions/search-action.php
@@ -0,0 +1,31 @@
+<?php
+require_once '../../src/db.php';
+session_start();
+// echo "testing";
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $searchItem = trim($_POST['searchBar']);
+    echo $searchItem;
+
+    $stmt = $pdo->prepare("SELECT users.id, users.first_name, users.last_name, users.country_id, users.subdivision_id, roles.name
+                            FROM users
+                            JOIN roles ON users.role_id = roles.id
+                            WHERE users.subdivision_id = :subdivision
+                        ");
+    
+    $stmt->execute([':subdivision' => $searchItem]);
+
+    $_SESSION['searchData'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (empty($_SESSION['searchData'])) {
+        // $_SESSION['searchError'] = "Sorry, we couldn't find any results. Please try Again.";
+        // header("Location: index.php?error=invalid_searach");
+        echo "testing";
+    } else {
+        echo "testing2";
+        header("Location: ../search-results.php");
+        exit;
+    }
+
+}
+
+?>
--- public/index.php
+++ public/index.php
@@ -29,10 +29,108 @@ unset($_SESSION["experienceId"]);
   <section class="hero">
     <div class="hero-content">
       <h1>GourmetDestination</h1>
-      <div class="search-bar">
-        <input type="text" placeholder="Search" />
+      <!-- <div class="search-bar"> -->
+        <!-- <input type="text" placeholder="Search" />
+        <button><span class="material-icons">search</span></button> -->
+        
+        <?php if (!empty($_GET['error'])) echo "<p>Sorry, there's no search results. Please try again.</p>"; ?>
+        <form class="search-bar" method="POST" action="actions/search-action.php">
         <button><span class="material-icons">search</span></button>
-      </div>
+        <input list="searchItem" name="searchBar" placeholder="Search by: Pronvinces and States">
+            <datalist id="searchItem">
+                <option value="AB">Alberta</option>
+                <option value="BC">British Columbia</option>
+                <option value="MB">Manitoba</option>
+                <option value="NB">New Brunswick</option>
+                <option value="NL">Newfoundland and Labrador</option>
+                <option value="NS">Nova Scotia</option>
+                <option value="ON">Ontario</option>
+                <option value="PE">Prince Edward Island</option>
+                <option value="QC">Quebec</option>
+                <option value="SK">Saskatchewan</option>
+                <option value="NT">Northwest Territories</option>
+                <option value="NU">Nunavut</option>
+                <option value="YT">Yukon</option>
+                <option value="AL">Alabama</option>
+                <option value="AK">Alaska</option>
+                <option value="AZ">Arizona</option>
+                <option value="AR">Arkansas</option>
+                <option value="CA">California</option>
+                <option value="CO">Colorado</option>
+                <option value="CT">Connecticut</option>
+                <option value="DE">Delaware</option>
+                <option value="DC">District Of Columbia</option>
+                <option value="FL">Florida</option>
+                <option value="GA">Georgia</option>
+                <option value="HI">Hawaii</option>
+                <option value="ID">Idaho</option>
+                <option value="IL">Illinois</option>
+                <option value="IN">Indiana</option>
+                <option value="IA">Iowa</option>
+                <option value="KS">Kansas</option>
+                <option value="KY">Kentucky</option>
+                <option value="LA">Louisiana</option>
+                <option value="ME">Maine</option>
+                <option value="MD">Maryland</option>
+                <option value="MA">Massachusetts</option>
+                <option value="MI">Michigan</option>
+                <option value="MN">Minnesota</option>
+                <option value="MS">Mississippi</option>
+                <option value="MO">Missouri</option>
+                <option value="MT">Montana</option>
+                <option value="NE">Nebraska</option>
+                <option value="NV">Nevada</option>
+                <option value="NH">New Hampshire</option>
+                <option value="NJ">New Jersey</option>
+                <option value="NM">New Mexico</option>
+                <option value="NY">New York</option>
+                <option value="NC">North Carolina</option>
+                <option value="ND">North Dakota</option>
+                <option value="OH">Ohio</option>
+                <option value="OK">Oklahoma</option>
+                <option value="OR">Oregon</option>
+                <option value="PA">Pennsylvania</option>
+                <option value="RI">Rhode Island</option>
+                <option value="SC">South Carolina</option>
+                <option value="SD">South Dakota</option>
+                <option value="TN">Tennessee</option>
+                <option value="TX">Texas</option>
+                <option value="UT">Utah</option>
+                <option value="VT">Vermont</option>
+                <option value="VA">Virginia</option>
+                <option value="WA">Washington</option>
+                <option value="WV">West Virginia</option>
+                <option value="WI">Wisconsin</option>
+                <option value="WY">Wyoming</option>
+                <!-- <button><span class="material-icons">search</span></button> -->
+                <?php
+                
+                //diplays existing users and their roles in the search bar
+                    // $stmt = $pdo->prepare("SELECT users.first_name, users.last_name, roles.name
+                    //                        FROM users
+                    //                        JOIN roles ON users.role_id = roles.id
+                    //                        ");
+                    // $stmt->execute();
+
+                    // while ($user = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    //     echo "<option value='" . htmlspecialchars($user['first_name']) . " " . htmlspecialchars($user['last_name']) . "'>"
+                    //                 . htmlspecialchars($user['name']) . "</option>";
+                                    
+                    // }
+
+                    //displays existing experiences
+                    // $stmt2 = $pdo->prepare("SELECT name, description FROM experiences");
+                    // $stmt2->execute();
+
+                    // while ($experience = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    //     echo "<option value='" . htmlspecialchars($experience['name']) . "'>"
+                    //             . htmlspecialchars($experience['description']) . "</option>";
+                    // }
+                    
+                ?>
+            </datalist>
+       </form>
+      <!-- </div> -->
     </div>
   </section>
 
