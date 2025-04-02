b7a11f70b9f36103bd8a8d8b9ae80a14424c382e
mizryl <pham_linda@hotmail.com>
2025-04-02 09:45:58 -0700

    Finish? Need testing with values tho

--- public/financial-report-action.php
+++ public/financial-report-action.php
@@ -9,7 +9,6 @@ if (empty($_SESSION['hostSub'])) {
     $hostSubscription = 0;
 }
 // Retrieve local guide & home chef subscription
-// $stmt = $pdo->prepare("SELECT * FROM transactions WHERE type = 'subscription' AND amount = :amount");
 $stmt = $pdo->prepare("SELECT SUM(amount) as total_amount, COUNT(*) AS total_count
                        FROM transactions 
                        WHERE type = 'subscription' AND amount = :amount");
@@ -18,44 +17,50 @@ $stmt->execute([':amount' => $hostSubscriptionTax]);
 $hostResults = $stmt->fetch(PDO::FETCH_ASSOC); 
 
 // Handle NULL cases, if no matching rows
-$hostGrossRevenue = $hostResults['total_amount'] ?? 0; //gross revenue
+$_SESSION['hostAmount'] = ($hostResults['total_amount'] ?? 0) /1.12; 
 $_SESSION['hostCount'] = $hostResults['total_count'] ?? 0 ;
-$_SESSION['hostRevenue'] = $hostGrossRevenue / 1.12; //divide by tax rate to get the net income
+$_SESSION['hostTotalRevenue'] =  $_SESSION['hostAmount'] * $_SESSION['hostCount'];
 
-
-// while ($_SESSION['hostSub'] = $stmt->fetch(PDO::FETCH_ASSOC)) {
-//     echo $hostSubscription += $_SESSION['hostSub']['amount'];
-// }
 
 // Retrieve tourist subscription, gross revenue, and count
-// $stmt2 = $pdo->prepare("SELECT * FROM transactions WHERE type = 'subscription' AND amount = :amount"); 
-// $stmt2->execute(['amount' => $touristSubscriotionTax]);
+$stmt2 = $pdo->prepare("SELECT SUM(amount) as total_amount, COUNT(*) AS total_count
+                       FROM transactions 
+                       WHERE type = 'subscription' AND amount = :amount");
 
-$stmt2 = $pdo->prepare("WITH filtered_data AS (
-                            SELECT amount, type
-                            FROM transactions
-                            WHERE amount = :amount
-                            )
-                            SELECT COUNT(*) FROM AS total_count, SUM(amount) AS total_amount
-                            FROM filtered_data;");
-
-$stmt2->execute(['amount' => $touristSubscriotionTax]);
+$stmt2->execute([':amount' => $touristSubscriotionTax]);
 
 $touristResults = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$_SESSION['touristAmount'] = $touristResults['total_amount'];
-$_SESSION['touristCount'] = $touristResults['total_amount'];
+// Validate query results for NULL
+    $_SESSION['touristAmount'] = ($touristResults['total_amount'] ?? 0) / 1.12;
+    $_SESSION['touristCount'] = $touristResults['total_count'] ?? 0;
+    $_SESSION['touristTotalRevenue'] = $_SESSION['touristAmount'] * $_SESSION['touristCount'];
 
-$touristGrossRevenue = $touristResults['total_amount'] ?? 0; //gross revenue
-$_SESSION['touristCount'] = $hostResults['total_count'] ?? 0 ;
-$_SESSION['touristRevenue'] = $touristGrossRevenue / 1.12;
+// Retrieve total number of bookings, revenue
+$stmt3 = $pdo->prepare("SELECT COUNT(*) as total_count, SUM(amount) AS total_amount
+                        FROM transactions
+                        WHERE type = 'booking'
+                        ");
+$stmt3->execute();
+$bookingResults = $stmt3->fetch(PDO::FETCH_ASSOC);
+
+$_SESSION['bookingAmount'] = ($bookingtResults['total_amount'] ?? 0) / 1.12;
+$_SESSION['bookingCount'] = $bookingResults['total_count'] ?? 0;
+
+// fetch the commission calculation for each booking in a seperate loop 
+$stmt3->execute();
+
+while ($bookingResults = $stmt3->fetch(PDO::FETCH_ASSOC)) {
+    $_SESSION['bookingCommission'] += ($bookingResults['amount'] * 0.15);
+}
+
+$_SESSION['totalRevenue'] = $_SESSION['hostTotalRevenue'] + $_SESSION['touristTotalRevenue'] + $_SESSION['bookingCommission'];
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
     header("Location: financial-report.php");
 }
 
 ?>
-
 
 
 
--- public/financial-report.php
+++ public/financial-report.php
@@ -8,6 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
     <div class="financial-report">
@@ -18,22 +19,53 @@ session_start();
         <table>
             <tr>
                 <th>TOTAL USERS SUBSCRIBED</th>
-                <th>VALUES</th>
+                <td><strong>VALUES</strong></td>
             </tr>
             <tr>
-                <td>Local Guide & Home Chef</td>
-                <td><?php echo $_SESSION['hostCount'];?></td>
+                <th class="subheading">Local Guide & Home Chef</th>
+                <td><?php echo $_SESSION['hostCount']?></td>
             </tr>
             <tr>
-                <td>Tourist</td>
-                <td><?php echo $_SESSION['touristCount'];?></td>
+            <th class="subheading">Tourist</th>
+                <td><?php echo $_SESSION['touristCount']?></td>
             </tr>
             <tr></tr>
             <tr>
-                <th>REVENUE</th>
-                <th></th>
+                <th>SUBSCRIPTION REVENUE</th>
             </tr>
-
+            <tr>
+                <th class="subheading">Local Guide & Home Chef Subscription</th>
+                <td><?php echo $_SESSION['hostTotalRevenue']?></td>
+            </tr>
+            <tr>
+                <th class="subheading">Tourist Subscription</th>
+                <td><?php echo $_SESSION['touristTotalRevenue']?></td>
+            </tr> 
+            <tr></tr>
+            <tr>
+                <th>BOOKING & COMISSION REVENUE</th>
+            </tr>
+            <tr>
+                <th class="subheading">Total Number of Bookings</th>
+                <td><?php echo $_SESSION['bookingCount']?></td>
+            </tr>
+            <tr>
+                <th class="subheading">Total Accumulative Value of Booking</th>
+                <td><?php echo $_SESSION['bookingAmount']?></td>
+            </tr>
+            <tr>
+                <th class="subheading">Commission Rate</th>
+                <td>15%</td>
+            </tr>
+            <tr>
+                <th class="subheading">Total Commission Revenue</th>
+                <td><?php echo $_SESSION['bookingCommission']?></td>
+            </tr>
+            <tr></tr>
+            <tr>
+                <th>TOTAL REVENUE</th>
+                <td><?php echo $_SESSION['totalRevenue']?></td>
+            </tr>
         </table>
     </div>
 </body>
