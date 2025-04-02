85ca3aa9a132b689db6214a76598f5bd7ca988ec
haminpants <samueltfong03@gmail.com>
2025-04-02 12:37:34 -0700

    force non-admin users back to index.php

--- public/financial-report.php
+++ public/financial-report.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (empty($_SESSION["userData"]["roleId"]) || $_SESSION["userData"]["roleId"] !== 0) {
+    header("Location: index.php");
+    die();
+}
 ?>
 
 <!DOCTYPE html>
